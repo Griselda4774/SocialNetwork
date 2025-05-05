c1e84e8854033817545300264c6c27c8798d343b
Tam Tran <trandinhtam4774@gmail.com>
2025-05-05 09:17:47 +0700

    add server-discovery

---
+++ server-discovery/src/test/java/com/socialnetwork/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
